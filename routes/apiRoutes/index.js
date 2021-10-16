41c7f483983cd167845ad13c654600b10c83c3ae
ggamb <glenngambardella372@gmail.com>
2021-10-16 19:00:17 -0400

    updates to functions

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const { notes } = require('../../lib/db/db.json');
 
-
+console.log(notes);
 router.get('/notes', (req, res) => {
     let results = notes;
+    console.log(res.notes[0]);
+    console.log(res.json(notes)[0]);
+    console.log(res.json(notes));
     
 });
 
