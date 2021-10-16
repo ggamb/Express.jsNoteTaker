6cda1c9a06d798d54cf9b43e660144d2b2c2e052
ggamb <glenngambardella372@gmail.com>
2021-10-16 18:45:28 -0400

    working server'

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,19 +1,14 @@
 const router = require('express').Router();
-const { notes } = require('../lib/public/notes');
+const { notes } = require('../../lib/db/db.json');
 
-router.get('../../lib/public/notes', (req, res) => {
+
+router.get('/notes', (req, res) => {
     let results = notes;
-    /*
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);*/
+    
 });
 
-router.post('../../lib/public/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
-
+router.post('/notes', (req, res) => {
+    
     
 });
   
