087e9d9b944203a5320ed57a2bb9215b0482a908
ggamb <glenngambardella372@gmail.com>
2021-10-16 14:10:14 -0400

    added get and post functions

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,12 +1,20 @@
-const path = require('path');
 const router = require('express').Router();
+const { notes } = require('../lib/public/notes');
 
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+router.get('../../lib/public/notes', (req, res) => {
+    let results = notes;
+    /*
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);*/
 });
 
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
+router.post('../../lib/public/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
 
+    
+});
+  
 module.exports = router;
