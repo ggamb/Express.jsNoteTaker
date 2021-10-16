4492343804572d83c08cd8e1d391909504fe5da5
ggamb <glenngambardella372@gmail.com>
2021-10-16 19:39:42 -0400

    committing files

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const { notes } = require('../../lib/db/db.json');
 
-console.log(notes);
+
 router.get('/notes', (req, res) => {
-    let results = notes;
-    console.log(res.notes[0]);
-    console.log(res.json(notes)[0]);
     console.log(res.json(notes));
     
 });
